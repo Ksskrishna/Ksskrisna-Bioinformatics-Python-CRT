ca7f8a9da391bd6a417add04822008460132dfe2
Krishna <221FA14058@vignan.ac.in>
2025-06-03 16:47:16 +0530

    "Added code to read an integer value and find summation of even and odd digits"

--- 3-06-2025/code14.py
+++ 3-06-2025/code14.py
@@ -1,1 +1,12 @@
 #write a py prog to read an integer value and find number of 0's present in user entered number
+num = int(input())
+count = 0
+if(num==0):
+    print("1")
+else:
+    while(num!=0):
+        digits = num%10
+        if(digits == 0):
+            count = count+1
+        num = num//10
+    print(count)
