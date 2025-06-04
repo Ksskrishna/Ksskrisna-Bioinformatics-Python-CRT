99c4e29162b71e1c61027a7e43830150e161b77d
Krishna <221FA14058@vignan.ac.in>
2025-06-04 23:25:41 +0530

    assignment completed

--- 4-06-2025/assignment.py
+++ 4-06-2025/assignment.py
@@ -1,2 +1,9 @@
 #write a python prog to reverse a list of numbers without using reverse method 
-#write a python prog to sort a list of numbers wihtout using sort method
+num = int(input())
+list1=[]
+temp=[]
+for i in range(num):
+    temp=int(input())
+    list1.append(temp)
+print(f"without reversing {list1}")
+print(f"after reversing {list1[::-1]}")
