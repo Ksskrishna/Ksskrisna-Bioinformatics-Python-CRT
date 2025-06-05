3db440435213f7dd316ab572c8fa127153a247be
Krishna <221FA14058@vignan.ac.in>
2025-06-05 11:55:02 +0530

    day 3 of coding

---
+++ 5-06-2025/assignment.py
@@ -0,0 +1,3 @@
+#write a py prog to read 2 integer values as input from the user 
+#1. add 2 numbers without using addition operator and without predefined methods and functions
+#2. substract without using - operator and without using predefined methods and functions
