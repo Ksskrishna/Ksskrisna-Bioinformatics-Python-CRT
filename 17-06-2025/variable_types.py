e0af3f8bfc3ad2cc63b539d2fdcbd9a1d0ca0ebb
Krishna <221FA14058@vignan.ac.in>
2025-06-17 21:31:26 +0530

    oops concepts learned

---
+++ 17-06-2025/variable_types.py
@@ -0,0 +1,12 @@
+'''
+Global variables: (2 types) --> class variable (static), instance variables (non static variables)
+
+    --->    A variable which is declared within the class outside of a method,
+            Function, Cunstructor, or Block is known as GLobal Variable
+    --->    GLobal variable has global scope where you can access the variable through out the program
+    --->    It has a global access
+
+Local variables:
+    --->    A variable which is declared within the class inside of a method, function, cunstroctor or block is known as local variable
+    --->    Local variable 
+'''
