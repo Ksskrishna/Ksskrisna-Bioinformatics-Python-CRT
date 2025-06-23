32abd3c9af8c7b1146102f0813429768f253f28f
Krishna <221FA14058@vignan.ac.in>
2025-06-23 14:49:24 +0530

    none

--- 20-06-20205/node.py
+++
@@ -1,5 +0,0 @@
-class node:
-    def __init__(self,data):
-        new_node = node
-        if self.head == None:
-            self.head = new_node
--- 21-06-2025/insert at end.py
+++
@@ -1,9 +0,0 @@
-def insert_end (self,data):
-    new = Node(data)
-    if not self.head:
-        self.head = new
-        return
-    temp = self.head
-    while temp.next:
-        temp = temp.next
-    temp.next = new
